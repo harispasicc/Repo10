197dea8d0a161e22e5e144c5c38023e770a26600
Haris Pasic <harispasic@ymail.com>
2022-04-06 17:55:35 +0200

    Objects

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,14 @@
+var myPet = {
+    species: "Husky",
+    name: "Rex",
+    legs: 4,
+    friends: ["Muki", "Lea"]
+}
 
+function myFunction(myObj){
+    return myObj;
+}
+
+console.log(myFunction(myPet));
+
+module.exports = { myPet, myFunction };
